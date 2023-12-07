2b1a61981c1851e718cdb1cb6f0b84fb23391239
gaeuni <rkdms0116@github.com>
2023-12-06 18:58:30 -0600

    modify add

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { 
-    res.send('Hello gaeun's Jenkins!')
+    res.send('Hello gaeun's Jenkins!, Good Job!')
 });
 
 const port = process.env.PORT || 5000;
